6ea4a98b3bcfd1946358c9fac67572efa915221c
smplyvishal <simplyvishal88@gmail.com>
2025-12-18 23:18:21 +0530

    Add products data to products.js

---
+++ data/products.js
@@ -0,0 +1,38 @@
+// data/products.js
+const products = [
+  {
+    id: 1,
+    name: "Handmade Crochet Bag",
+    price: 1200,
+    category: "toys",
+    image: "product1.jpg"
+  },
+  {
+    id: 2,
+    name: "Crochet Octopus",
+    price: 900,
+    category: "toys",
+    image: "product2.jpg"
+  },
+  {
+    id: 3,
+    name: "Heart Keychain",
+    price: 150,
+    category: "keychains",
+    image: "product3.jpg"
+  },
+  {
+    id: 4,
+    name: "Name Keychain",
+    price: 200,
+    category: "keychains",
+    image: "product4.jpg"
+  },
+  {
+    id: 5,
+    name: "Cute Fridge Magnet",
+    price: 180,
+    category: "magnets",
+    image: "product5.jpg"
+  }
+];
